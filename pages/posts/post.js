f50a58b493892ca6dadd69b020605b92a73e25e7
aramais21 <aramayis.yeghiazaryan@picsart.com>
2021-03-15 17:40:27 +0400

    added images

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,11 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 const Post = () => {
     return(
+        <>
         <h1>
             Post
             <Link href="/home" >go to home</Link>
         </h1>
+        <div>
+            <img src="/images/logo.png" alt = 'logo' />
+        </div>
+        <div>
+            <Image
+                src="/images/logo.png" // Route of the image file
+                height={453} // Desired size with correct aspect ratio
+                width={820} // Desired size with correct aspect ratio
+                alt="logo"
+            />
+        </div>
+        </>
     )
 }
 
